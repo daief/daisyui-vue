19a8985ef6990b6f395e0633a4524d8bd0563cde
daief <daief@users.noreply.github.com>
2022-03-30 15:19:01 +0000

    deploy: 33b76e9f2d451fb9273fdd718e8be4fc59487479

---
+++ assets/demo.1c4a0a91.js
@@ -0,0 +1,12 @@
+import{u as h,y as n,z as g,c as a,A as s,F as r,B as t,C as u,a as l}from"./vendor.dc484497.js";const _=t("div",{class:"markdown-body"},[t("h1",null,"Demo for development")],-1),j={class:"text-center"},i=l("Click"),v=l("Item 1"),w=l("Item 2"),f=t("div",{class:"markdown-body"},null,-1),B=[],I={setup(b,{expose:p}){return p({frontmatter:{meta:[]}}),h({meta:[]}),(x,y)=>{const c=n("dv-button"),e=n("dv-menu-item"),o=n("dv-menu"),d=n("dv-dropdown"),m=n("Playground");return u(),g(r,null,[_,a(m,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-dropdown</span> <span class="hljs-attr">triggerAction</span>=<span class="hljs-string">&quot;click&quot;</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>Click<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">template</span> #<span class="hljs-attr">content</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">dv-menu</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;shadow-lg rounded-lg bg-white p-2 w-56&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-menu-item</span>&gt;</span>Item 1<span class="hljs-tag">&lt;/<span class="hljs-name">dv-menu-item</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-menu-item</span>&gt;</span>Item 2<span class="hljs-tag">&lt;/<span class="hljs-name">dv-menu-item</span>&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">dv-menu</span>&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">template</span>&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">dv-dropdown</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:s(()=>[t("div",j,[a(d,{triggerAction:"click"},{content:s(()=>[a(o,{class:"shadow-lg rounded-lg bg-white p-2 w-56"},{default:s(()=>[a(e,null,{default:s(()=>[v]),_:1}),a(e,null,{default:s(()=>[w]),_:1})]),_:1})]),default:s(()=>[a(c,null,{default:s(()=>[i]),_:1})]),_:1})])]),_:1}),f],64)}}};export{I as default,B as meta};
