45de8b5c3c4de0e449dd11256b5115944dc6d251
daief <1437931235@qq.com>
2022-09-22 23:22:42 +0800

    wip: save

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (api) => {
-  api.cache(true);
-  return {
-    presets: ['@babel/preset-typescript'],
-    // https://github.com/vuejs/jsx-next
-    plugins: ['@vue/babel-plugin-jsx'],
-  };
-};
