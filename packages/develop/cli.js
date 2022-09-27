861ee25061e69302aa0ec1d5995c732f1e23b9d9
daief <1437931235@qq.com>
2022-09-27 20:47:05 +0800

    chore: develop add ts-node

--- packages/develop/cli.js
+++ packages/develop/cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-require('./dist/cli');
+const register = () => {
+  const tsNode = require('ts-node');
+  const path = require('path');
+
+  tsNode.register({
+    project: path.resolve(__dirname, 'tsconfig.json'),
+  });
+};
+
+register();
+
+require('./src/cli.ts');
