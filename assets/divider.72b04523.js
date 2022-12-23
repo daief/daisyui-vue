67a3c8b39f6ff10f5ef24dfdb8491f9689ee9ef5
daief <daief@users.noreply.github.com>
2022-12-23 07:21:43 +0000

    deploy: 7dabc97fcc0aee4afeeec0d692743aa935ed7742

---
+++ assets/divider.72b04523.js
@@ -0,0 +1,45 @@
+import{u as o,a3 as e,a5 as i,c as a,a6 as n,a7 as s,F as r,a8 as h,a as c}from"./vendor.faf17d9b.js";const g=s("div",{class:"markdown-body"},[s("h1",null,"Divider"),s("h2",null,"Examples"),s("p",null,"divider")],-1),j={class:"flex flex-col w-full"},u=s("div",{class:"grid h-20 card bg-base-300 rounded-xl place-items-center"}," content ",-1),m=c("OR"),v=s("div",{class:"grid h-20 card bg-base-300 rounded-xl place-items-center"}," content ",-1),_=s("div",{class:"markdown-body"},[s("p",null,"divider horizontal")],-1),x={class:"flex flex-row w-full"},f=s("div",{class:"grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center"}," content ",-1),b=c("OR"),w=s("div",{class:"grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center"}," content ",-1),q=s("div",{class:"markdown-body"},[s("p",null,"divider with no text")],-1),k={class:"flex flex-col w-full"},y=s("div",{class:"grid h-20 card bg-base-300 rounded-xl place-items-center"}," content ",-1),z=s("div",{class:"grid h-20 card bg-base-300 rounded-xl place-items-center"}," content ",-1),I=s("div",{class:"markdown-body"},[s("p",null,"divider with no text")],-1),O={class:"flex flex-row w-full"},R=s("div",{class:"grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center"}," content ",-1),B=s("div",{class:"grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center"}," content ",-1),D={class:"markdown-body"},N=s("h2",null,"Divider",-1),T=s("h3",null,"Attributes",-1),A=[],H={setup(V,{expose:p}){return p({frontmatter:{meta:[]}}),o({meta:[]}),(F,M)=>{const l=e("dv-divider"),t=e("Playground"),d=e("MarkdownTable");return h(),i(r,null,[g,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-col w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card bg-base-300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span>&gt;</span>OR<span class="hljs-tag">&lt;/<span class="hljs-name">dv-divider</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card bg-base-300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",j,[u,a(l,null,{default:n(()=>[m]),_:1}),v])]),_:1}),_,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-row w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span> <span class="hljs-attr">horizontal</span>&gt;</span>OR<span class="hljs-tag">&lt;/<span class="hljs-name">dv-divider</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",x,[f,a(l,{horizontal:""},{default:n(()=>[b]),_:1}),w])]),_:1}),q,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-col w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card bg-base-300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card bg-base-300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",k,[y,a(l),z])]),_:1}),I,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-row w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span> <span class="hljs-attr">horizontal</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card bg-base-300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",O,[R,a(l,{horizontal:""}),B])]),_:1}),s("div",D,[N,T,a(d,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1},{title:"type",dataIndex:2},{title:"default",dataIndex:3}],dataSource:[{"0":"horizontal","1":"Divide elements next to each other","2":"boolean","3":"false"}]})])],64)}}};export{H as default,A as meta};
---
+++ assets/input.6d151be7.js
@@ -0,0 +1,62 @@
+import{u as e,a3 as p,a5 as r,c as a,a6 as t,a7 as s,F as h,a8 as u}from"./vendor.faf17d9b.js";const i=s("div",{class:"markdown-body"},[s("h1",null,"Input"),s("h2",null,"Examples"),s("p",null,"Text input with border")],-1),d={class:"text-center"},j=s("div",{class:"markdown-body"},[s("p",null,"Text input without border")],-1),g={class:"text-center"},q=s("div",{class:"markdown-body"},[s("p",null,"Ghost (no background)")],-1),m={class:"text-center"},x=s("div",{class:"markdown-body"},[s("p",null,"Input with colors")],-1),v={class:"text-center"},w=s("div",{class:"py-1"},null,-1),_=s("div",{class:"py-1"},null,-1),f=s("div",{class:"py-1"},null,-1),y=s("div",{class:"py-1"},null,-1),b=s("div",{class:"py-1"},null,-1),z=s("div",{class:"py-1"},null,-1),k=s("div",{class:"markdown-body"},[s("p",null,"Input size")],-1),T={class:"text-center"},I=s("div",{class:"py-1"},null,-1),B=s("div",{class:"py-1"},null,-1),C=s("div",{class:"py-1"},null,-1),E=s("div",{class:"markdown-body"},[s("p",null,"Disabled")],-1),F={class:"text-center"},M={class:"markdown-body"},N=s("h2",null,"Input",-1),P=s("h3",null,"Attributes",-1),J=[],K={setup(S,{expose:c}){return c({frontmatter:{meta:[]}}),e({meta:[]}),(D,G)=>{const n=p("dv-input"),l=p("Playground"),o=p("MarkdownTable");return u(),r(h,null,[i,a(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;Type here&quot;</span> /&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:t(()=>[s("div",d,[a(n,{class:"w-full max-w-xs",placeholder:"Type here"})])]),_:1}),j,a(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">:border</span>=<span class="hljs-string">&quot;false&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;Type here&quot;</span> /&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:t(()=>[s("div",g,[a(n,{class:"w-full max-w-xs",border:!1,placeholder:"Type here"})])]),_:1}),q,a(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+    <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;ghost&quot;</span>
+    <span class="hljs-attr">:border</span>=<span class="hljs-string">&quot;false&quot;</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span>
+    <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;Type here&quot;</span>
+  /&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:t(()=>[s("div",m,[a(n,{variant:"ghost",border:!1,class:"w-full max-w-xs",placeholder:"Type here"})])]),_:1}),x,a(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+    <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;primary&quot;</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span>
+    <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;primary color&quot;</span>
+  /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+    <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;secondary&quot;</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span>
+    <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;secondary color&quot;</span>
+  /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+    <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;accent&quot;</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span>
+    <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;accent color&quot;</span>
+  /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;info&quot;</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;info color&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+    <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;success&quot;</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span>
+    <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;success color&quot;</span>
+  /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+    <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;warning&quot;</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span>
+    <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;warning color&quot;</span>
+  /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">variant</span>=<span class="hljs-string">&quot;error&quot;</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;error color&quot;</span> /&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:t(()=>[s("div",v,[a(n,{variant:"primary",class:"w-full max-w-xs",placeholder:"primary color"}),w,a(n,{variant:"secondary",class:"w-full max-w-xs",placeholder:"secondary color"}),_,a(n,{variant:"accent",class:"w-full max-w-xs",placeholder:"accent color"}),f,a(n,{variant:"info",class:"w-full max-w-xs",placeholder:"info color"}),y,a(n,{variant:"success",class:"w-full max-w-xs",placeholder:"success color"}),b,a(n,{variant:"warning",class:"w-full max-w-xs",placeholder:"warning color"}),z,a(n,{variant:"error",class:"w-full max-w-xs",placeholder:"error color"})])]),_:1}),k,a(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;xs&quot;</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;xs size&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;sm&quot;</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;sm size&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;md&quot;</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;md size&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-1&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;lg&quot;</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;lg size&quot;</span> /&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:t(()=>[s("div",T,[a(n,{size:"xs",class:"w-full max-w-xs",placeholder:"xs size"}),I,a(n,{size:"sm",class:"w-full max-w-xs",placeholder:"sm size"}),B,a(n,{size:"md",class:"w-full max-w-xs",placeholder:"md size"}),C,a(n,{size:"lg",class:"w-full max-w-xs",placeholder:"lg size"})])]),_:1}),E,a(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full max-w-xs&quot;</span> <span class="hljs-attr">placeholder</span>=<span class="hljs-string">&quot;disabled&quot;</span> <span class="hljs-attr">disabled</span> /&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:t(()=>[s("div",F,[a(n,{class:"w-full max-w-xs",placeholder:"disabled",disabled:""})])]),_:1}),s("div",M,[N,P,a(o,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1},{title:"type",dataIndex:2},{title:"default",dataIndex:3}],dataSource:[{"0":"variant","1":"input color type","2":"neutral, primary, secondary, accent, info, success, warning, error, ghost","3":"neutral"},{"0":"size","1":"input size","2":"ISize","3":"md"},{"0":"border","1":"input border","2":"boolean","3":"true"},{"0":"v-model","1":"v-model for input value","2":"string","3":"-"}]})])],64)}}};export{K as default,J as meta};
