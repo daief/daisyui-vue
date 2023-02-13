e36cc150d09a9b35503f16e48b7307aa821dcdab
daief <daief@users.noreply.github.com>
2023-02-13 13:55:49 +0000

    deploy: 461b28b8488f67f7be23548b7e7b95e685f29a1e

---
+++ assets/demo.fc2fd14c.js
@@ -0,0 +1,35 @@
+import{K as r,c as s,a as n,a5 as t,F as e,u as h,a7 as u,a8 as d,a9 as p,aa as j}from"./vendor.284b0716.js";var g={setup:()=>{r("1");const l=[1,2,3];return()=>s(t("dv-space"),null,{default:()=>[n("I'm a text"),null,!1,0,"a",s(t("dv-button"),null,{default:()=>[n("button")]}),s(e,null,[l.map(a=>s("div",null,[a])),n("asd"),l.map(a=>s("div",null,[a,n("2")]))])]})}};const m=p("div",{class:"markdown-body"},[p("h1",null,"Demo for development")],-1),i=p("div",{class:"markdown-body"},null,-1),y=[],b={setup(l,{expose:a}){return a({frontmatter:{meta:[]}}),h({meta:[]}),(_,v)=>{const c=g,o=t("Playground");return j(),u(e,null,[m,s(o,{lang:"tsx",code:`<span class="hljs-keyword">import</span> { <span class="hljs-title class_">Tabs</span>, <span class="hljs-title class_">TabPanel</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;daisyui-vue&#x27;</span>;
+<span class="hljs-keyword">import</span> { ref } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;vue&#x27;</span>;
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">const</span> currentTab = <span class="hljs-title function_">ref</span>(<span class="hljs-string">&#x27;1&#x27;</span>);
+    <span class="hljs-keyword">const</span> slots = {
+      <span class="hljs-attr">default</span>: <span class="hljs-function">() =&gt;</span> <span class="hljs-string">&#x27;Content 3&#x27;</span>,
+      <span class="hljs-attr">title</span>: <span class="hljs-function">() =&gt;</span> <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">a</span> <span class="hljs-attr">style</span>=<span class="hljs-string">&quot;color: blue&quot;</span>&gt;</span>Title Slot<span class="hljs-tag">&lt;/<span class="hljs-name">a</span>&gt;</span></span>,
+    };
+
+    <span class="hljs-keyword">const</span> arr = [<span class="hljs-number">1</span>, <span class="hljs-number">2</span>, <span class="hljs-number">3</span>];
+
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span>&gt;</span>
+        I&#x27;m a text
+        {null}
+        {false}
+        {0}
+        {&#x27;a&#x27;}
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+        <span class="hljs-tag">&lt;&gt;</span>
+          {arr.map((it) =&gt; (
+            <span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>{it}<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+          ))}
+          asd
+          {arr.map((it) =&gt; (
+            <span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>{it}2<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+          ))}
+        <span class="hljs-tag">&lt;/&gt;</span></span>
+      &lt;/dv-space&gt;
+    );
+  },
+};
+`},{default:d(()=>[s(c)]),_:1}),i],64)}}};export{b as default,y as meta};
---
+++ assets/space.d3a5bb42.js
@@ -0,0 +1,84 @@
+import{K as v,c as s,a5 as l,a as t,aa as h,a7 as i,ac as _,a9 as a,a8 as n,ad as b,F as j,ae as k,af as q,u as z,ag as I}from"./vendor.284b0716.js";import{_ as C}from"./app.42068e1e.js";var S={setup:()=>{const p=["xs","sm","md","lg"].map(e=>({label:e,value:e})),c=v("md");return()=>s("div",{class:"space-y-4"},[s(l("dv-radio-group"),{options:p,modelValue:c.value,"onUpdate:modelValue":e=>c.value=e},null),s("br",null,null),s(l("dv-space"),{direction:"vertical",spacing:c.value},{default:()=>[t("I'm a text"),s(l("dv-button"),null,{default:()=>[t("button")]}),s(l("dv-button"),null,{default:()=>[t("button2")]})]})])}};const A={},V=p=>(k("data-v-5098191f"),p=p(),q(),p),B={class:"space-align-block"},$=t("button"),N=V(()=>a("div",{class:"mock-block"},"block",-1));function T(p,c){const e=l("dv-button"),g=l("dv-space");return h(),i(j,null,_(["center","start","end","baseline"],r=>a("div",B,[s(g,{align:r},{default:n(()=>[t(b(r)+" ",1),s(e,null,{default:n(()=>[$]),_:1}),N]),_:2},1032,["align"])])),64)}var E=C(A,[["render",T],["__scopeId","data-v-5098191f"]]),F={setup:()=>{const p=v("8px");return()=>s("div",{class:"space-y-4"},[s("div",null,[s(l("dv-input"),{value:p.value,onInput:c=>p.value=c.target.value},null)]),s(l("dv-space"),{direction:"vertical",spacing:p.value},{default:()=>[t("I'm a text"),s(l("dv-button"),null,{default:()=>[t("button")]}),s(l("dv-button"),null,{default:()=>[t("button2")]})]})])}};const M=a("div",{class:"markdown-body"},[a("h1",null,"Space"),a("h2",null,"Examples"),a("p",null,"Space")],-1),P=t(" I'm a text "),D=t("button"),H=a("div",{class:"markdown-body"},[a("p",null,"Vertical")],-1),K=t(" I'm a text "),L=t("button"),U=t("button2"),W=a("div",{class:"markdown-body"},[a("p",null,"Spacing size")],-1),G=a("div",{class:"markdown-body"},[a("p",null,"Align")],-1),J=a("div",{class:"markdown-body"},[a("p",null,"Custom spacing")],-1),O=a("div",{class:"markdown-body"},[a("p",null,"Wrap")],-1),Q={class:"markdown-body"},R=a("h2",null,"Space",-1),X=a("h3",null,"Attributes",-1),as=[],ns={setup(p,{expose:c}){return c({frontmatter:{meta:[]}}),z({meta:[]}),(r,Y)=>{const u=l("dv-button"),d=l("dv-space"),o=l("Playground"),x=S,f=E,y=F,w=l("MarkdownTable");return h(),i(j,null,[M,s(o,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span>&gt;</span>
+  I&#x27;m a text
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">dv-space</span>&gt;</span>
+`},{default:n(()=>[s(d,null,{default:n(()=>[P,s(u,null,{default:n(()=>[D]),_:1})]),_:1})]),_:1}),H,s(o,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span> <span class="hljs-attr">direction</span>=<span class="hljs-string">&quot;vertical&quot;</span>&gt;</span>
+  I&#x27;m a text
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button2<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">dv-space</span>&gt;</span>
+`},{default:n(()=>[s(d,{direction:"vertical"},{default:n(()=>[K,s(u,null,{default:n(()=>[L]),_:1}),s(u,null,{default:n(()=>[U]),_:1})]),_:1})]),_:1}),W,s(o,{lang:"tsx",code:`<span class="hljs-keyword">import</span> { ref } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;vue&#x27;</span>;
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">const</span> options = [<span class="hljs-string">&#x27;xs&#x27;</span>, <span class="hljs-string">&#x27;sm&#x27;</span>, <span class="hljs-string">&#x27;md&#x27;</span>, <span class="hljs-string">&#x27;lg&#x27;</span>].<span class="hljs-title function_">map</span>(<span class="hljs-function">(<span class="hljs-params">s</span>) =&gt;</span> ({
+      <span class="hljs-attr">label</span>: s,
+      <span class="hljs-attr">value</span>: s,
+    }));
+    <span class="hljs-keyword">const</span> size = <span class="hljs-title function_">ref</span>(<span class="hljs-string">&#x27;md&#x27;</span>);
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;space-y-4&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-radio-group</span> <span class="hljs-attr">options</span>=<span class="hljs-string">{options}</span> <span class="hljs-attr">v-model</span>=<span class="hljs-string">{size.value}</span> /&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">br</span> /&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span> <span class="hljs-attr">direction</span>=<span class="hljs-string">&quot;vertical&quot;</span> <span class="hljs-attr">spacing</span>=<span class="hljs-string">{size.value}</span>&gt;</span>
+          I&#x27;m a text
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button2<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+        <span class="hljs-tag">&lt;/<span class="hljs-name">dv-space</span>&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+    );
+  },
+};
+`},{default:n(()=>[s(x)]),_:1}),G,s(o,{lang:"html",code:`<span class="hljs-tag">&lt;<span class="hljs-name">template</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;space-align-block&quot;</span>
+    <span class="hljs-attr">v-for</span>=<span class="hljs-string">&quot;(align) in [&#x27;center&#x27;, &#x27;start&#x27;, &#x27;end&#x27;, &#x27;baseline&#x27;]&quot;</span>
+  &gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span> <span class="hljs-attr">:align</span>=<span class="hljs-string">&quot;align&quot;</span>&gt;</span>
+      {{align}}
+      <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;mock-block&quot;</span>&gt;</span>block<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">dv-space</span>&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">template</span>&gt;</span>
+<span class="hljs-tag">&lt;<span class="hljs-name">style</span> <span class="hljs-attr">scoped</span>&gt;</span><span class="language-css">
+  <span class="hljs-selector-class">.space-align-block</span> {
+    <span class="hljs-attribute">flex</span>: none;
+    <span class="hljs-attribute">margin</span>: <span class="hljs-number">8px</span> <span class="hljs-number">4px</span>;
+    <span class="hljs-attribute">padding</span>: <span class="hljs-number">4px</span>;
+    <span class="hljs-attribute">border</span>: <span class="hljs-number">1px</span> solid <span class="hljs-number">#40a9ff</span>;
+  }
+  <span class="hljs-selector-class">.mock-block</span> {
+    <span class="hljs-attribute">display</span>: inline-block;
+    <span class="hljs-attribute">padding</span>: <span class="hljs-number">32px</span> <span class="hljs-number">8px</span> <span class="hljs-number">16px</span>;
+    <span class="hljs-attribute">background</span>: <span class="hljs-built_in">rgba</span>(<span class="hljs-number">150</span>, <span class="hljs-number">150</span>, <span class="hljs-number">150</span>, <span class="hljs-number">0.2</span>);
+  }
+</span><span class="hljs-tag">&lt;/<span class="hljs-name">style</span>&gt;</span>
+`},{default:n(()=>[s(f)]),_:1}),J,s(o,{lang:"tsx",code:`<span class="hljs-keyword">import</span> { ref } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;vue&#x27;</span>;
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">const</span> size = <span class="hljs-title function_">ref</span>(<span class="hljs-string">&#x27;8px&#x27;</span>);
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;space-y-4&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-input</span>
+            <span class="hljs-attr">value</span>=<span class="hljs-string">{size.value}</span>
+            <span class="hljs-attr">onInput</span>=<span class="hljs-string">{(e)</span> =&gt;</span> (size.value = e.target.value)}
+          /&gt;
+        <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span> <span class="hljs-attr">direction</span>=<span class="hljs-string">&quot;vertical&quot;</span> <span class="hljs-attr">spacing</span>=<span class="hljs-string">{size.value}</span>&gt;</span>
+          I&#x27;m a text
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span>&gt;</span>button2<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+        <span class="hljs-tag">&lt;/<span class="hljs-name">dv-space</span>&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+    );
+  },
+};
+`},{default:n(()=>[s(y)]),_:1}),O,s(o,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">dv-space</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-64 max-w-full&quot;</span> <span class="hljs-attr">block</span> <span class="hljs-attr">wrap</span> <span class="hljs-attr">:spacing</span>=<span class="hljs-string">&quot;[12, 4]&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span> <span class="hljs-attr">v-for</span>=<span class="hljs-string">&quot;(i) in Array.from(new Array(20).keys())&quot;</span> <span class="hljs-attr">:key</span>=<span class="hljs-string">&quot;i&quot;</span>&gt;</span>
+    {{i}}
+  <span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">dv-space</span>&gt;</span>
+`},{default:n(()=>[s(d,{class:"w-64 max-w-full",block:"",wrap:"",spacing:[12,4]},{default:n(()=>[(h(!0),i(j,null,_(Array.from(new Array(20).keys()),m=>(h(),I(u,{key:m},{default:n(()=>[t(b(m),1)]),_:2},1024))),128))]),_:1})]),_:1}),a("div",Q,[R,X,s(w,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1},{title:"type",dataIndex:2},{title:"default",dataIndex:3}],dataSource:[{"0":"align","1":"align","2":"start, end, center, baseline","3":"-"},{"0":"direction","1":"direction","2":"vertical, horizontal","3":"horizontal"},{"0":"wrap","1":"items wrap or not","2":"boolean","3":"false"},{"0":"block","1":"block or not","2":"boolean","3":"false"},{"0":"spacing","1":"items spacing","2":"ISize, string, number","3":"md"}]})])],64)}}};export{ns as default,as as meta};
