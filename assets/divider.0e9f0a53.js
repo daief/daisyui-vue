edd12ad91674dceb2ba804354c6e5df89d9fffce
daief <daief@users.noreply.github.com>
2023-05-19 14:07:49 +0000

    deploy: f0da19b3dab212bb4e783d4fa89917dc9360e48b

---
+++ assets/divider.0e9f0a53.js
@@ -0,0 +1,45 @@
+import{u as o,b as e,d as i,c as a,w as n,e as s,F as r,o as h,a as c}from"./app.834882cb.js";const g=s("div",{class:"markdown-body"},[s("h1",null,"Divider"),s("h2",null,"Examples"),s("p",null,"divider")],-1),j={class:"flex flex-col w-full"},v=s("div",{class:"grid h-20 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),u=c("OR"),m=s("div",{class:"grid h-20 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),_=s("div",{class:"markdown-body"},[s("p",null,"divider horizontal")],-1),x={class:"flex flex-row w-full"},f=s("div",{class:"grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),b=c("OR"),w=s("div",{class:"grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),q=s("div",{class:"markdown-body"},[s("p",null,"divider with no text")],-1),k={class:"flex flex-col w-full"},y=s("div",{class:"grid h-20 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),z=s("div",{class:"grid h-20 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),I=s("div",{class:"markdown-body"},[s("p",null,"divider with no text")],-1),O={class:"flex flex-row w-full"},R=s("div",{class:"grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),B=s("div",{class:"grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center"}," content ",-1),D={class:"markdown-body"},N=s("h2",null,"Divider",-1),T=s("h3",null,"Attributes",-1),A=[],H={setup(V,{expose:d}){return d({frontmatter:{meta:[]}}),o({meta:[]}),(F,M)=>{const l=e("dv-divider"),t=e("Playground"),p=e("MarkdownTable");return h(),i(r,null,[g,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-col w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card dv-bgbase300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span>&gt;</span>OR<span class="hljs-tag">&lt;/<span class="hljs-name">dv-divider</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card dv-bgbase300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",j,[v,a(l,null,{default:n(()=>[u]),_:1}),m])]),_:1}),_,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-row w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span> <span class="hljs-attr">horizontal</span>&gt;</span>OR<span class="hljs-tag">&lt;/<span class="hljs-name">dv-divider</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",x,[f,a(l,{horizontal:""},{default:n(()=>[b]),_:1}),w])]),_:1}),q,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-col w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card dv-bgbase300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid h-20 card dv-bgbase300 rounded-xl place-items-center&quot;</span>&gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",k,[y,a(l),z])]),_:1}),I,a(t,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;flex flex-row w-full&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">dv-divider</span> <span class="hljs-attr">horizontal</span> /&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+    <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;grid flex-grow h-32 card dv-bgbase300 rounded-xl place-items-center&quot;</span>
+  &gt;</span>
+    content
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+`},{default:n(()=>[s("div",O,[R,a(l,{horizontal:""}),B])]),_:1}),s("div",D,[N,T,a(p,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1},{title:"type",dataIndex:2},{title:"default",dataIndex:3}],dataSource:[{0:"horizontal",1:"Divide elements next to each other",2:"boolean",3:"false"}]})])],64)}}};export{H as default,A as meta};
