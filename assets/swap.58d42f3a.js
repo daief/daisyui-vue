7c46b85decf1fd14e0702327a28ebb84f7aa28d9
daief <daief@users.noreply.github.com>
2023-05-21 13:59:45 +0000

    deploy: d7ba6780da6bf6988b0805a08bdb4cf752c6510e

---
+++ assets/swap.58d42f3a.js
@@ -0,0 +1,48 @@
+import{c as s,b as n,r as g,a as e,u as m,d as f,w as t,e as a,F as v,o as x}from"./app.fdd71efb.js";var _={setup:()=>()=>s(n("dv-swap"),{on:()=>s(n("IconVolumeHigh"),{size:"48px"},null),off:()=>s(n("IconVolumeMute"),{size:"48px"},null)},null)},w={setup:()=>()=>s("div",{class:"text-center text-9xl"},[s(n("dv-swap"),{on:"\u{1F608}",off:"\u{1F607}",animation:"flip"},null)])},y={setup:()=>{const p=g(!1);return()=>s("div",{class:"text-center"},[s("div",{class:"py-2"},[s(n("dv-button"),{onClick:()=>p.value=!0},{default:()=>[e("on")]}),e("\xA0/\xA0"),s(n("dv-button"),{onClick:()=>p.value=!1},{default:()=>[e("off")]})]),s("div",{class:"text-9xl"},[s(n("dv-swap"),{on:"\u{1F973}",off:"\u{1F62D}",animation:"flip",active:p.value},null)])])}};const q=a("div",{class:"markdown-body"},[a("h1",null,"Swap"),a("h2",null,"Examples"),a("p",null,"Swap text")],-1),b=a("div",{class:"markdown-body"},[a("p",null,"Swap volume icons")],-1),k=a("div",{class:"markdown-body"},[a("p",null,"Swap icons with rotate effect")],-1),I=a("div",{class:"markdown-body"},[a("p",null,"Swap with flip effect")],-1),z=a("div",{class:"markdown-body"},[a("p",null,"Controlled swap")],-1),C={class:"markdown-body"},S=a("h2",null,"Swap",-1),F=a("h3",null,"Attributes",-1),M=a("h3",null,"Solts",-1),H=[],T={setup(p,{expose:h}){return h({frontmatter:{meta:[]}}),m({meta:[]}),(O,A)=>{const c=n("dv-swap"),l=n("Playground"),r=_,u=n("IconSunny"),i=n("IconMoon"),j=w,d=y,o=n("MarkdownTable");return x(),f(v,null,[q,s(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">dv-swap</span> <span class="hljs-attr">on</span>=<span class="hljs-string">&quot;ON&quot;</span> <span class="hljs-attr">off</span>=<span class="hljs-string">&quot;OFF&quot;</span> /&gt;</span>
+`},{default:t(()=>[s(c,{on:"ON",off:"OFF"})]),_:1}),b,s(l,{lang:"tsx",code:`<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">dv-swap</span>
+        <span class="hljs-attr">on</span>=<span class="hljs-string">{()</span> =&gt;</span> <span class="hljs-tag">&lt;<span class="hljs-name">IconVolumeHigh</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;48px&quot;</span> /&gt;</span>}
+        off={() =&gt; <span class="hljs-tag">&lt;<span class="hljs-name">IconVolumeMute</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;48px&quot;</span> /&gt;</span>}
+      /&gt;</span>
+    );
+  },
+};
+`},{default:t(()=>[s(r)]),_:1}),k,s(l,{code:`<span class="hljs-tag">&lt;<span class="hljs-name">dv-swap</span> <span class="hljs-attr">animation</span>=<span class="hljs-string">&quot;rotate&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">template</span> <span class="hljs-attr">v-slot:on</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">IconSunny</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;48px&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">template</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">template</span> <span class="hljs-attr">v-slot:off</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">IconMoon</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;48px&quot;</span> /&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">template</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">dv-swap</span>&gt;</span>
+`},{default:t(()=>[s(c,{animation:"rotate"},{on:t(()=>[s(u,{size:"48px"})]),off:t(()=>[s(i,{size:"48px"})]),_:1})]),_:1}),I,s(l,{lang:"tsx",code:`<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center text-9xl&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-swap</span> <span class="hljs-attr">on</span>=<span class="hljs-string">&quot;\u{1F608}&quot;</span> <span class="hljs-attr">off</span>=<span class="hljs-string">&quot;\u{1F607}&quot;</span> <span class="hljs-attr">animation</span>=<span class="hljs-string">&quot;flip&quot;</span> /&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+    );
+  },
+};
+`},{default:t(()=>[s(j)]),_:1}),z,s(l,{lang:"tsx",code:`<span class="hljs-keyword">import</span> { ref } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;vue&#x27;</span>;
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">const</span> active = <span class="hljs-title function_">ref</span>(<span class="hljs-literal">false</span>);
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-center&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;py-2&quot;</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span> <span class="hljs-attr">onClick</span>=<span class="hljs-string">{()</span> =&gt;</span> (active.value = true)}&gt;on<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+          <span class="hljs-symbol">&amp;nbsp;</span>/<span class="hljs-symbol">&amp;nbsp;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-button</span> <span class="hljs-attr">onClick</span>=<span class="hljs-string">{()</span> =&gt;</span> (active.value = false)}&gt;off<span class="hljs-tag">&lt;/<span class="hljs-name">dv-button</span>&gt;</span>
+        <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;text-9xl&quot;</span>&gt;</span>
+          <span class="hljs-tag">&lt;<span class="hljs-name">dv-swap</span> <span class="hljs-attr">on</span>=<span class="hljs-string">&quot;\u{1F973}&quot;</span> <span class="hljs-attr">off</span>=<span class="hljs-string">&quot;\u{1F62D}&quot;</span> <span class="hljs-attr">animation</span>=<span class="hljs-string">&quot;flip&quot;</span> <span class="hljs-attr">active</span>=<span class="hljs-string">{active.value}</span> /&gt;</span>
+        <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+    );
+  },
+};
+`},{default:t(()=>[s(d)]),_:1}),a("div",C,[S,F,s(o,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1},{title:"type",dataIndex:2},{title:"default",dataIndex:3}],dataSource:[{0:"on",1:"render on",2:"-",3:"-"},{0:"off",1:"render off",2:"-",3:"-"},{0:"tag",1:"wrap elemnt tag",2:"string",3:"label"},{0:"animation",1:"animation effect",2:"rotate, flip",3:"-"},{0:"active",1:"swap active status",2:"boolean",3:"-"},{0:"onChange",1:"swap active status change event",2:"Function",3:"-"}]}),M,s(o,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1}],dataSource:[{0:"on",1:"render on"},{0:"off",1:"render off"}]})])],64)}}};export{T as default,H as meta};
---
+++ assets/table.cf1b2eb2.js
@@ -0,0 +1,190 @@
+import{c as s,b as p,_ as f,o,d as j,a as b,F as h,u as y,w as e,e as a,g as v}from"./app.fdd71efb.js";var _={setup:()=>{const n=[{title:"",dataIndex:"num",fixed:"left",width:20},{title:"name",dataIndex:"name"},{title:"job",dataIndex:"job"},{title:"favorite color",dataIndex:"favoriteColor"}],l=[{num:1,name:"Cy Ganderton",job:"Quality Control Specialist",favoriteColor:"Blue"},{num:2,name:"Hart Hagerty",job:"Desktop Support Technician",favoriteColor:"Purple"},{num:3,name:"Brice Swyre",job:"Tax Accountant",favoriteColor:"Red"},{num:4,name:"Marjy Ferencz",job:"Office Assistant I",favoriteColor:"Crimson"}];return()=>s("div",{class:"w-full"},[s(p("dv-table"),{columns:n,dataSource:l},null)])}},w={setup:()=>({columns:[{title:"",dataIndex:"num",fixed:"left",width:20},{title:"name",dataIndex:"name"},{title:"job",dataIndex:"job"},{title:"favorite color",dataIndex:"favoriteColor"}],dataSource:[{num:1,name:"Cy Ganderton",job:"Quality Control Specialist",favoriteColor:"Blue"},{num:2,name:"Hart Hagerty",job:"Desktop Support Technician",favoriteColor:"Purple"},{num:3,name:"Brice Swyre",job:"Tax Accountant",favoriteColor:"Red"},{num:4,name:"Marjy Ferencz",job:"Office Assistant I",favoriteColor:"Crimson"}]})};const C={class:"w-full"};function I(n,l,t,i,u,x){const c=p("dv-table");return o(),j("div",C,[s(c,{columns:n.columns,dataSource:n.dataSource,zebra:"",compact:""},null,8,["columns","dataSource"])])}var S=f(w,[["render",I]]),k={setup:()=>{const n=[{title:"",dataIndex:"num",fixed:"left",width:20,render:t=>s("strong",null,[t])},{title:"name",dataIndex:"name"},{title:"job",dataIndex:"job"},{title:()=>s(h,null,[s(p("IconColorPalette"),{size:"1.5em",style:"margin-right: 6px"},null),b("favorite color")]),dataIndex:"favoriteColor",render:t=>s("span",{style:{color:t}},[t])}],l=[{num:1,name:"Cy Ganderton",job:"Quality Control Specialist",favoriteColor:"Blue"},{num:2,name:"Hart Hagerty",job:"Desktop Support Technician",favoriteColor:"Purple"},{num:3,name:"Brice Swyre",job:"Tax Accountant",favoriteColor:"Red"},{num:4,name:"Marjy Ferencz",job:"Office Assistant I",favoriteColor:"Crimson"}];return()=>s("div",{class:"w-full"},[s(p("dv-table"),{columns:n,dataSource:l},null)])}};const z=a("div",{class:"markdown-body"},[a("h1",null,"Table"),a("h2",null,"Examples"),a("p",null,"table")],-1),T=a("div",{class:"markdown-body"},[a("p",null,"table-zebra & table-compact")],-1),q=a("div",{class:"markdown-body"},[a("p",null,"table-jsx")],-1),B={class:"markdown-body"},A=a("h2",null,"Table",-1),H=a("h3",null,"Attributes",-1),P=v(`<h3>ITableColumn</h3><pre><code class="hljs language-ts"><span class="hljs-keyword">interface</span> <span class="hljs-title class_">ITableColumn</span>&lt;T = <span class="hljs-built_in">unknown</span>&gt; {
+  title?: <span class="hljs-built_in">string</span> | (<span class="hljs-function">() =&gt;</span> <span class="hljs-built_in">any</span>);
+  dataIndex?: <span class="hljs-built_in">string</span>;
+  key?: <span class="hljs-built_in">string</span>;
+  fixed?: <span class="hljs-string">&#39;left&#39;</span> | <span class="hljs-string">&#39;right&#39;</span>;
+  render?: <span class="hljs-function">(<span class="hljs-params">text: <span class="hljs-built_in">string</span>, record: T, index: <span class="hljs-built_in">number</span></span>) =&gt;</span> <span class="hljs-built_in">any</span>;
+}
+</code></pre>`,2),M=[],D={setup(n,{expose:l}){return l({frontmatter:{meta:[]}}),y({meta:[]}),(u,x)=>{const c=_,r=p("Playground"),d=S,m=k,g=p("MarkdownTable");return o(),j(h,null,[z,s(r,{lang:"tsx",code:`<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">const</span> columns = [
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;num&#x27;</span>,
+        <span class="hljs-attr">fixed</span>: <span class="hljs-string">&#x27;left&#x27;</span>,
+        <span class="hljs-attr">width</span>: <span class="hljs-number">20</span>,
+      },
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;name&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;name&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;job&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;job&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;favorite color&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;favoriteColor&#x27;</span>,
+      },
+    ];
+
+    <span class="hljs-keyword">const</span> dataSource = [
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">1</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Cy Ganderton&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Quality Control Specialist&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Blue&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">2</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Hart Hagerty&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Desktop Support Technician&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Purple&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">3</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Brice Swyre&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Tax Accountant&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Red&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">4</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Marjy Ferencz&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Office Assistant I&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Crimson&#x27;</span>,
+      },
+    ];
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-table</span> <span class="hljs-attr">columns</span>=<span class="hljs-string">{columns}</span> <span class="hljs-attr">dataSource</span>=<span class="hljs-string">{dataSource}</span> /&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+    );
+  },
+};
+`},{default:e(()=>[s(c)]),_:1}),T,s(r,{lang:"html",code:`<span class="hljs-tag">&lt;<span class="hljs-name">template</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full&quot;</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">dv-table</span> <span class="hljs-attr">:columns</span>=<span class="hljs-string">&quot;columns&quot;</span> <span class="hljs-attr">:dataSource</span>=<span class="hljs-string">&quot;dataSource&quot;</span> <span class="hljs-attr">zebra</span> <span class="hljs-attr">compact</span> /&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">template</span>&gt;</span>
+
+<span class="hljs-tag">&lt;<span class="hljs-name">script</span> <span class="hljs-attr">lang</span>=<span class="hljs-string">&quot;tsx&quot;</span>&gt;</span><span class="language-javascript">
+  <span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+    <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+      <span class="hljs-keyword">const</span> columns = [
+        {
+          <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;&#x27;</span>,
+          <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;num&#x27;</span>,
+          <span class="hljs-attr">fixed</span>: <span class="hljs-string">&#x27;left&#x27;</span>,
+          <span class="hljs-attr">width</span>: <span class="hljs-number">20</span>,
+        },
+        {
+          <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;name&#x27;</span>,
+          <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;name&#x27;</span>,
+        },
+        {
+          <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;job&#x27;</span>,
+          <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;job&#x27;</span>,
+        },
+        {
+          <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;favorite color&#x27;</span>,
+          <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;favoriteColor&#x27;</span>,
+        },
+      ];
+
+      <span class="hljs-keyword">const</span> dataSource = [
+        {
+          <span class="hljs-attr">num</span>: <span class="hljs-number">1</span>,
+          <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Cy Ganderton&#x27;</span>,
+          <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Quality Control Specialist&#x27;</span>,
+          <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Blue&#x27;</span>,
+        },
+        {
+          <span class="hljs-attr">num</span>: <span class="hljs-number">2</span>,
+          <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Hart Hagerty&#x27;</span>,
+          <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Desktop Support Technician&#x27;</span>,
+          <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Purple&#x27;</span>,
+        },
+        {
+          <span class="hljs-attr">num</span>: <span class="hljs-number">3</span>,
+          <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Brice Swyre&#x27;</span>,
+          <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Tax Accountant&#x27;</span>,
+          <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Red&#x27;</span>,
+        },
+        {
+          <span class="hljs-attr">num</span>: <span class="hljs-number">4</span>,
+          <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Marjy Ferencz&#x27;</span>,
+          <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Office Assistant I&#x27;</span>,
+          <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Crimson&#x27;</span>,
+        },
+      ];
+      <span class="hljs-keyword">return</span> {
+        columns,
+        dataSource,
+      };
+    },
+  };
+</span><span class="hljs-tag">&lt;/<span class="hljs-name">script</span>&gt;</span>
+`},{default:e(()=>[s(d)]),_:1}),q,s(r,{lang:"tsx",code:`<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">setup</span>: <span class="hljs-function">() =&gt;</span> {
+    <span class="hljs-keyword">const</span> columns = [
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;num&#x27;</span>,
+        <span class="hljs-attr">fixed</span>: <span class="hljs-string">&#x27;left&#x27;</span>,
+        <span class="hljs-attr">width</span>: <span class="hljs-number">20</span>,
+        <span class="hljs-attr">render</span>: <span class="hljs-function">(<span class="hljs-params">num</span>) =&gt;</span> <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">strong</span>&gt;</span>{num}<span class="hljs-tag">&lt;/<span class="hljs-name">strong</span>&gt;</span></span>,
+      },
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;name&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;name&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-string">&#x27;job&#x27;</span>,
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;job&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">title</span>: <span class="hljs-function">() =&gt;</span> (
+          <span class="language-xml"><span class="hljs-tag">&lt;&gt;</span>
+            <span class="hljs-tag">&lt;<span class="hljs-name">IconColorPalette</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;1.5em&quot;</span> <span class="hljs-attr">style</span>=<span class="hljs-string">&quot;margin-right: 6px&quot;</span> /&gt;</span>
+            favorite color
+          <span class="hljs-tag">&lt;/&gt;</span></span>
+        ),
+        <span class="hljs-attr">dataIndex</span>: <span class="hljs-string">&#x27;favoriteColor&#x27;</span>,
+        <span class="hljs-attr">render</span>: <span class="hljs-function">(<span class="hljs-params">color</span>) =&gt;</span> <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">span</span> <span class="hljs-attr">style</span>=<span class="hljs-string">{{</span> <span class="hljs-attr">color</span> }}&gt;</span>{color}<span class="hljs-tag">&lt;/<span class="hljs-name">span</span>&gt;</span></span>,
+      },
+    ];
+
+    <span class="hljs-keyword">const</span> dataSource = [
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">1</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Cy Ganderton&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Quality Control Specialist&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Blue&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">2</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Hart Hagerty&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Desktop Support Technician&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Purple&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">3</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Brice Swyre&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Tax Accountant&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Red&#x27;</span>,
+      },
+      {
+        <span class="hljs-attr">num</span>: <span class="hljs-number">4</span>,
+        <span class="hljs-attr">name</span>: <span class="hljs-string">&#x27;Marjy Ferencz&#x27;</span>,
+        <span class="hljs-attr">job</span>: <span class="hljs-string">&#x27;Office Assistant I&#x27;</span>,
+        <span class="hljs-attr">favoriteColor</span>: <span class="hljs-string">&#x27;Crimson&#x27;</span>,
+      },
+    ];
+    <span class="hljs-keyword">return</span> <span class="hljs-function">() =&gt;</span> (
+      <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;w-full&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">dv-table</span> <span class="hljs-attr">columns</span>=<span class="hljs-string">{columns}</span> <span class="hljs-attr">dataSource</span>=<span class="hljs-string">{dataSource}</span> /&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+    );
+  },
+};
+`},{default:e(()=>[s(m)]),_:1}),a("div",B,[A,H,s(g,{columns:[{title:"name",dataIndex:0},{title:"description",dataIndex:1},{title:"type",dataIndex:2},{title:"default",dataIndex:3}],dataSource:[{0:"columns",1:"table columns config",2:"ITableColumn[]",3:"-"},{0:"dataSource",1:"table data source",2:"array",3:"-"},{0:"compact",1:"makes table more compact",2:"boolean",3:"-"},{0:"zebra",1:"makes table rows zebra",2:"boolean",3:"-"}]}),P])],64)}}};export{D as default,M as meta};
